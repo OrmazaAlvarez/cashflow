e98749d86120050b16de852cd394dbcb3820e91b
Lenin Antonio Ormaza Alvarez <lenin.ormaza@gmail.com>
2022-04-21 02:02:40 -0500

    HOMEPAGE: Crendo el componente HomePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <LayoutPage>
+    <template #header>
+      <HeaderPage />
+    </template>
+  </LayoutPage>
+</template>
+<script lang="ts">
+import LayoutPage from "@/components/LayoutPage.vue";
+import HeaderPage from "@/components/HeaderPage.vue";
+
+export default {
+  components: {
+    LayoutPage,
+    HeaderPage,
+  },
+};
+</script>
